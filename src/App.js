325dcdd2e57f3153a42d2d83cb40dda0315f1432
elyse502 <elyseniyibizi502@gmail.com>
2024-07-29 10:39:28 +0300

    Use React Styled-Components to style our Components 🎨

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import UsersList from "./components/UsersList";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import styled, { createGlobalStyle } from "styled-components";
 
+const BackgroundColor = createGlobalStyle`
+  body {
+    background-color: ${props => (props.light ? '#f2f2f2' : '#333')};
+  }
+`;
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// CamelCase
+const BlueText = styled.div`
+  color: blue;
+`;
+
+const WhiteText = styled.div`
+  color: #fff;
+`;
 
 export default() => {
   return (
     <>
-      <Container fluid className='mt-4 mb-3'>
-        <UsersList />
-      </Container>
+      <BackgroundColor light />
+        <Container fluid className='mt-4 mb-3'>
+          <BlueText>
+            <UsersList />
+          </BlueText>
+        </Container>
     </>
   );
 }
