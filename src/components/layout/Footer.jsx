93b828b2b855ca40f33bf31ecb3b45440096c446
elyse502 <elyseniyibizi502@gmail.com>
2024-08-08 16:35:37 +0200

    Enhance footer little CSS/HTML tricks 🗺

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,8 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (
         <Container>
-            <Row className="justify-content-center">
+            <hr />
+            <Row className="mt-5 justify-content-center">
                 <Col md={{ span: 3, offset: 1 }}>
                     <h4>User</h4>
                     <NavLink className="text-muted fw-bold" to='/create'>Create A User</NavLink>
